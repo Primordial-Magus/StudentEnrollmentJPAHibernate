108fb82acd5cb7a0525401f0b3a25c3f510848c2
Eric Tran <erictran0007@gmail.com>
2024-02-12 13:41:54 +1100

    Added config files for project

---
+++ src/main/java/com/eric/rizz/RizzApplication.java
@@ -0,0 +1,13 @@
+package com.eric.rizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eric/rizz/RizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.rizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
